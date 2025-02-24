650935af91899cd90a27f932252c7976c6e7860f
Javier Troya <javiertroya@gmail.com>
2025-02-24 19:36:15 +0100

    Rename UserHooks to User.

--- app/Hooks/User.php
+++ app/Hooks/User.php
@@ -4,7 +4,7 @@ namespace App\Hooks;
 
 use App\Hooks\User\Api;
 
-class UserHooks {
+class User {
     public static function init() : void {
         add_action('rest_api_init', Api::register_rest_route(...));
     }
